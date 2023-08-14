991e423084f0af2a77ad26550efcfe229dc7e783
joseph heath <heathj873@gmail.com>
2023-08-14 17:15:21 -0400

    first

---
+++ DeckofCardsLab/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using DeckofCardsLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckofCardsLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+        
+        public IActionResult Deck()
+        {
+            DeckModel result = DeckModelDAL.GetDeck();
+            if (result.remaining <= 0)
+            {
+                DeckModelDAL.ShuffleDeck();
+            }
+                return View(result);
+        }
+
+        public IActionResult ShuffleDeck()
+        {
+            
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckofCardsLab/Models/DeckModelDAL.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace DeckofCardsLab.Models
+{
+    public class DeckModelDAL
+    {
+        public static DeckModel GetDeck()//adjust here return type and name
+        {
+            //adjust url
+            //setup section
+            string url = "https://deckofcardsapi.com/api/deck/vvqrg5jwmw1l/draw/?count=5";
+
+            //request for data
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+            //converting to json
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd();
+
+            //adjust model type
+            //convert to c#
+            //install Newtonsoft.json
+            DeckModel result = JsonConvert.DeserializeObject<DeckModel>(json);
+            return result;
+        }
+        public static Shuffle ShuffleDeck()//adjust here return type and name
+        {
+            //adjust url
+            //setup section
+            string url = "https://deckofcardsapi.com/api/deck/vvqrg5jwmw1l/shuffle/";
+
+            //request for data
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+            //converting to json
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd();
+
+            //adjust model type
+            //convert to c#
+            //install Newtonsoft.json
+            Shuffle result = JsonConvert.DeserializeObject<Shuffle>(json);
+            return result;
+        }
+
+
+    }
+}
